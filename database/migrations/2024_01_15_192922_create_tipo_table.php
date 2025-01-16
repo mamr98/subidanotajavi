183b3866e2843fa6965255da211acdd5efa983c2
mamr98 <mmr0119@alu.medac.es>
2025-01-16 19:55:24 +0100

    tablas Tipo y Muestras hechas

--- database/migrations/2024_01_15_192922_create_tipo_table.php
+++ database/migrations/2024_01_15_192922_create_tipo_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('codigo');
             $table->string('nombre');
             $table->timestamps();
+            
         });
     }
 
