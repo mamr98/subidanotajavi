6dbbbca72b7ef5f4bdbb3268ca1a4dfaa7d8567e
PabloTorr03 <pablogdlt03@gmail.com>
2025-01-30 23:39:33 +0100

    UsuarioController crud hecho

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,12 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    function index(){
-        return view('usuarios');
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $usuario= new Usuario();
+        $usuario->email = $request['email'];
+        $usuario->password = $request['password'];
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        $usuarios = Usuario::all();
+        return response()->json($usuarios);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $email)
+    {
+        $usuario = Usuario::find($email);
+        $usuario->email = $request['email'];
+        $usuario->password = $request['password'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $email)
+    {
+        $usuario = Usuario::find($email);
+        $usuario->delete(); 
     }
 }
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Usuario extends Model
 {
-    //
+    protected $table ="usuario";
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+    
+    use HasFactory;
 }
