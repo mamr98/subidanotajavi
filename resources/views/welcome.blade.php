c1cc3fdca8d05f55d8c4673b007fca9e62b2c6e3
Marcoide120 <marcosgarciacasta5000@gmail.com>
2025-02-04 16:29:25 +0100

    vista formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,33 +6,53 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body class="bg-slate-400 min-h-screen flex items-center justify-center">
-  <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-4xl">
+<body class="bg-slate-400">
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        
+    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <a href="{{ route('logout') }}"><button type="button" class="text-white font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cerrar Sesion</button>
+      </button></a>
+    </div>
+      <div class="flex-1 flex justify-center md:order-1">
+            @isset($email)
+            <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">Has iniciado sesion con {{$email}}</span>
+            @endisset
+        </div>
+    </div>
+  </nav>
+
+  <h1 class=" bg-slate-400 flex items-center text-5xl mt-32 justify-center font-bold">Generar informes</h1>
+<div class="min-h-screen flex items-center justify-center">
+  <div class="bg-white p-6 rounded-lg shadow-lg mb-44 w-full max-w-4xl">
       <form class="grid grid-cols-1 md:grid-cols-2 gap-4">
           <!-- Código de la muestra -->
           <div>
               <label for="codigo" class=" block text-sm font-bold text-sky-800">Código de la muestra:</label>
-              <input type="text" id="codigo" name="codigo" placeholder="Código" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+              <input type="text" id="codigo" name="codigo" placeholder="Código" class="mt-1 block w-96  rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
           </div>
 
           <!-- Fecha de recolección -->
           <div>
               <label for="fecha" class=" ml-4 block text-sm font-bold text-sky-800">Fecha de recolección:</label>
-              <input type="date" id="fecha" name="fecha" class=" ml-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+              <input type="date" id="fecha" name="fecha" class=" ml-4 mt-1 block w-96 rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
           </div>
 
           <!-- Naturaleza de la muestra -->
           <div>
               <label for="naturaleza" class="block text-sm font-bold text-sky-800">Naturaleza de la muestra:</label>
-              <select id="naturaleza" name="naturaleza" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
-                  <option>Tipo</option>
+              <select id="naturaleza" name="naturaleza" class="mt-1 block w-96 rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                  <option></option>
+                  <option></option>
+                  <option></option>
+                  <option></option>
               </select>
           </div>
 
           <!-- Opciones biopsia -->
           <div>
               <label for="biopsia" class=" ml-4 block text-sm font-bold text-sky-800">Opciones biopsia:</label>
-              <select id="biopsia" name="biopsia" class=" ml-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+              <select id="biopsia" name="biopsia" class=" ml-4 mt-1 block w-96  rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
                   <option>Órgano</option>
               </select>
           </div>
@@ -40,7 +60,7 @@
           <!-- Conservación de muestra -->
           <div>
               <label for="conservacion" class="block text-sm font-bold text-sky-800">Conservación de muestra:</label>
-              <select id="conservacion" name="conservacion" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+              <select id="conservacion" name="conservacion" class="mt-1 block w-96  rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
                   <option>Formato</option>
               </select>
           </div>
@@ -48,7 +68,7 @@
           <!-- Centro de procedencia -->
           <div>
               <label for="centro" class=" ml-4 block text-sm font-bold text-sky-800">Centro de procedencia:</label>
-              <select id="centro" name="centro" class=" ml-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
+              <select id="centro" name="centro" class=" ml-4 mt-1 block w-96  rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500">
                   <option>Sede</option>
               </select>
           </div>
@@ -66,6 +86,7 @@
           </div>
       </form>
   </div>
+</div>
 </body>
 @vite(['resources/css/app.css'])
 </html>
@@ -74,11 +95,8 @@
     @foreach ($muestras as $m)
       <p>{{ $m->id }} {{ $m->fecha }} {{ $m->tipo->nombre }}</p>
     @endforeach --}}
-  @isset($email)
-        <h1>Has iniciado Sesion con {{$email}}</h1>
-  @endisset
+ 
 
 
-  <a href="{{ route('logout') }}"><button>Cerrar Sesion</button></a>
+{{--   <a href="{{ route('logout') }}"><button>Cerrar Sesion</button></a> --}}
   
-{{--   <button id="cerrar_sesion">Cerrar Sesion</button> --}}
