6ed2aa1279811d66b28a4b59d5848c500a36ac21
Davpp24 <pelaezperezdavid@gmail.com>
2025-02-05 20:38:36 +0100

    muestras

--- app/Http/Controllers/MuestraController.php
+++ app/Http/Controllers/MuestraController.php
@@ -10,4 +10,28 @@ class MuestraController extends Controller
         return view('welcome');
 
     }
+
+    public function create(Request $request)
+    {
+        $muestra= new Muestra();
+        $muestra->fecha = $request->input('fecha');
+        $muestra->codigo = $request->codigo('fecha');
+        $muestra->organo = $request->input('organo');
+        $muestra->idTipo = $request->input('idTipo');
+        $muestra->idFormato = $request->input('idFormato');
+        $muestra->idCalidad = $request->input('idCalidad');
+        $muestra->idUsuario = $request->input('idUsuario');
+        $muestra->idSede = $request->input('idSede');
+    
+        $muestra->save();
+
+        return response()->json(['mensaje' => 'Usuario creado correctamente'], 201);
+
+    }
+
+    public function show()
+    {
+        $muestras = Muestras::all();
+        return view('muestrasadmin')->with(['muestras'=>$muestras,]);
+    }
 }
