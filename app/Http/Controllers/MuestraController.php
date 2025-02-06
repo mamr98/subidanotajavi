32c4de246975f478ecbd5e858635b67c6343d743
Davpp24 <pelaezperezdavid@gmail.com>
2025-02-06 01:59:45 +0100

    crear y eliminar muestras de usuario normal

--- app/Http/Controllers/MuestraController.php
+++ app/Http/Controllers/MuestraController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sede;
+use App\Models\Tipo;
+use App\Models\Calidad;
+use App\Models\Formato;
+use App\Models\Muestra;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class MuestraController extends Controller
@@ -12,26 +18,88 @@ class MuestraController extends Controller
     }
 
     public function create(Request $request)
+{
+    $muestra = new Muestra();
+    $muestra->fecha = $request->input('fecha');
+    $muestra->codigo = $request->input('codigo');
+    $muestra->organo = $request->input('organo');
+    $muestra->idTipo = $request->input('idTipo');
+    $muestra->idFormato = $request->input('idFormato');
+    $muestra->idCalidad = $request->input('idCalidad');
+    $muestra->idUsuario = $request->input('idUsuario');
+    $muestra->idSede = $request->input('idSede');
+
+    $muestra->save();
+
+    return response()->json(['mensaje' => 'Muestra creada correctamente'], 201);
+}
+
+
+    public function show()
     {
-        $muestra= new Muestra();
+        $muestras = Muestra::all();
+        return view('listamuestras')->with(['muestras'=>$muestras]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $muestra = Muestra::find($id);
+    
         $muestra->fecha = $request->input('fecha');
-        $muestra->codigo = $request->codigo('fecha');
+        $muestra->codigo = $request->input('codigo');
         $muestra->organo = $request->input('organo');
         $muestra->idTipo = $request->input('idTipo');
         $muestra->idFormato = $request->input('idFormato');
         $muestra->idCalidad = $request->input('idCalidad');
         $muestra->idUsuario = $request->input('idUsuario');
         $muestra->idSede = $request->input('idSede');
-    
+
+        
         $muestra->save();
-
-        return response()->json(['mensaje' => 'Usuario creado correctamente'], 201);
-
+    
+        return response()->json(['mensaje' => 'Muestra actualizado correctamente'], 200);
     }
 
-    public function show()
+    public function destroy($id)
     {
-        $muestras = Muestras::all();
-        return view('muestrasadmin')->with(['muestras'=>$muestras,]);
+        $muestra = Muestra::where('id', $id)->first();
+        $muestra->delete(); 
+        return response()->json(['mensaje' => 'Muestra eliminada correctamente'], 201);
+    }
+
+    public function tipo($id)
+    {
+        $tipo = Tipo::where('id', $id)->first();
+        return $tipo;
+    }
+
+    public function formato($id)
+    {
+        $formato = Formato::where('id', $id)->first();
+        return $formato;
+    }
+
+    public function calidad($id)
+    {
+        $calidad = Calidad::where('id', $id)->first();
+        return $calidad;
+    }
+
+    public function usuario($id)
+    {
+        $usuario = Usuario::where('id', $id)->first();
+        return $usuario;
+    }
+
+    public function sede($id)
+    {
+        $sede = Sede::where('id', $id)->first();
+        return $sede;
+    }
+
+    public function muestra($id){
+        $muestra = Muestra::where('id', $id)->first();
+        return response()->json($muestra,200);
     }
 }
+
---
+++ resources/js/muestras.js
@@ -0,0 +1,379 @@
+import Swal from 'sweetalert2';
+
+const crearMuestra = document.querySelector('#crear_muestra');
+const modificar = document.querySelectorAll('.modificar');
+const eliminar = document.querySelectorAll('.eliminar');
+const modal_add = document.querySelector('#modal_add')
+const modal_update = document.getElementById('modal_update');
+ modal_add.style.visibility="hidden"
+ modal_update.style.visibility="hidden"
+
+ crearMuestra.addEventListener('click', () => {
+     modal_add.style.visibility = "";
+ 
+     Swal.fire({
+         title: 'Introduce los datos de la muestra',
+         html: rendermodal_add(),
+         confirmButtonText: "Guardar",
+         showCancelButton: true,
+     }).then((result) => {
+         if (result.isConfirmed) {
+            const fecha =  modal_add.querySelector('#fecha').value
+            const codigo =  modal_add.querySelector('#codigo').value
+            const organo = modal_add.querySelector("#organo")
+            const selectedOption = organo.options[organo.selectedIndex];
+            const valueOrgano = selectedOption.value;
+            const idTipo =  modal_add.querySelector('#idTipo').value
+            const idFormato =  modal_add.querySelector('#idFormato').value
+            const idCalidad =  modal_add.querySelector('#idCalidad').value
+            const idUsuario =  modal_add.querySelector('#idUsuario').value
+            const idSede =  modal_add.querySelector('#idSede').value
+
+            console.log(fecha+codigo+valueOrgano+idTipo+idFormato+idCalidad+idUsuario+idSede)
+ 
+             const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+ 
+             fetch('listamuestras/create', {
+                 method: "POST",
+                 headers: {
+                     'Content-Type': 'application/json',
+                     'X-CSRF-TOKEN': token
+                 },
+                 body: JSON.stringify({
+                     fecha: fecha,
+                     codigo: codigo,
+                     organo: valueOrgano,
+                     idTipo: idTipo,
+                     idFormato: idFormato,
+                     idCalidad: idCalidad,
+                     idUsuario: idUsuario,
+                     idSede: idSede,
+                 })
+             })
+             .then(respuesta => {
+                 if (!respuesta.ok) {
+                     return respuesta.text().then(err => {
+                         try {
+                             const jsonError = JSON.parse(err);
+                             throw new Error(jsonError.message || 'Error en la solicitud');
+                         } catch (e) {
+                             throw new Error(err || 'Error en la solicitud');
+                         }
+                     });
+                 }
+                 return respuesta.json();
+             })
+             .then(data => {
+                 console.log(data);
+ 
+                 // Mostrar mensaje de éxito
+                 Swal.fire('Éxito', 'Muestra creada correctamente', 'success');
+ 
+
+                 location.reload();
+             })
+             .catch(error => {
+                 console.error("Error en la petición:", error);
+                 Swal.fire('Error', error.message, 'error');
+             });
+         }
+     });
+ });
+
+ function rendermodal_add(){
+    const fecha =  modal_add.querySelector('#fecha')
+    const codigo =  modal_add.querySelector('#codigo')
+    const organo =  modal_add.querySelector('#organo')
+    const idTipo =  modal_add.querySelector('#idTipo')
+    const idFormato =  modal_add.querySelector('#idFormato')
+    const idCalidad =  modal_add.querySelector('#idCalidad')
+    const idUsuario =  modal_add.querySelector('#idUsuario')
+    const idSede =  modal_add.querySelector('#idSede')
+ 
+    return modal_add
+ }
+
+ 
+ 
+
+ function cargarDatos() {
+
+    const filas = document.querySelectorAll('#mostrar_muestras tr');
+
+    filas.forEach(fila => {
+        const tipo = fila.querySelector('.tipo');
+    const formato = fila.querySelector('.formato');
+    const calidad = fila.querySelector('.calidad');
+    const usuario = fila.querySelector('.usuario');
+    const sede = fila.querySelector('.sede');
+  
+    const idTipo = tipo.id;
+    console.log( tipo.id)
+    const idFormato = formato.id;
+    const idCalidad = calidad.id;
+    const idUsuario = usuario.id;
+    const idSede = sede.id;
+  
+    fetch(`listamuestras/tipo/${idTipo}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos de muestras:', data);        
+  
+        tipo.textContent = data.nombre; // Asegúrate de que 'data' tenga la propiedad 'nombre'
+      })
+      .catch(error => {
+        console.error('Error al obtener las muestras:', error);
+      });
+
+      fetch(`listamuestras/formato/${idFormato}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos de muestras:', data);        
+  
+        formato.textContent = data.nombre; // Asegúrate de que 'data' tenga la propiedad 'nombre'
+      })
+      .catch(error => {
+        console.error('Error al obtener las muestras:', error);
+      });
+
+      fetch(`listamuestras/calidad/${idCalidad}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos de muestras:', data);        
+  
+        calidad.textContent = data.nombre; // Asegúrate de que 'data' tenga la propiedad 'nombre'
+      })
+      .catch(error => {
+        console.error('Error al obtener las muestras:', error);
+      });
+
+      fetch(`listamuestras/usuario/${idUsuario}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos de muestras:', data);        
+  
+        usuario.textContent = data.email; // Asegúrate de que 'data' tenga la propiedad 'nombre'
+      })
+      .catch(error => {
+        console.error('Error al obtener las muestras:', error);
+      });
+
+      fetch(`listamuestras/sede/${idSede}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos de muestras:', data);        
+  
+        sede.textContent = data.nombre; // Asegúrate de que 'data' tenga la propiedad 'nombre'
+      })
+      .catch(error => {
+        console.error('Error al obtener las muestras:', error);
+      });
+    });
+    
+  }
+  
+  window.addEventListener('DOMContentLoaded', cargarDatos);
+
+
+  modificar.forEach(boton => {
+    boton.addEventListener('click', () => {
+        modal_update.style.visibility = ""; // O modal_update.style.display = "block";
+
+        const id = boton.id;
+        console.log("ID a enviar al fetch:", id);
+
+        fetch(`muestra/${id}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        })
+        .then(respuesta => {
+            if (!respuesta.ok) {
+                return respuesta.json().then(err => {
+                    throw new Error(err.message || `Error ${respuesta.status} en la solicitud`);
+                }).catch(() => {
+                    throw new Error(`Error ${respuesta.status} en la solicitud`);
+                });
+            }
+            return respuesta.json();
+        })
+        .then(data => {
+            console.log("Datos recibidos:", data);
+
+            Swal.fire({
+                title: 'Introduce Los datos que quieres modificar',
+                html: rendermodal_update(data),
+                confirmButtonText: "Actualizar",
+                showCancelButton: true,
+                didOpen: () => { // Asegúrate de que los campos estén disponibles después de que se abre el modal
+                    const btnActualizar = document.querySelector('.swal2-confirm'); // Obtén el botón "Actualizar" de SweetAlert
+
+                    if (btnActualizar) {
+                        btnActualizar.addEventListener('click', (event) => {
+                            event.preventDefault(); // Previene la recarga de la página
+
+                            const fecha = modal_update.querySelector('#fecha2').value; // Usa modal_update y los IDs correctos
+                            const codigo = modal_update.querySelector('#codigo2').value;
+                            const organoSelect = modal_update.querySelector("#organo2");
+                            const valueOrgano = organoSelect.options[organoSelect.selectedIndex].value;
+                            const idTipo = modal_update.querySelector('#idTipo2').value;
+                            const idFormato = modal_update.querySelector('#idFormato2').value;
+                            const idCalidad = modal_update.querySelector('#idCalidad2').value;
+                            const idUsuario = modal_update.querySelector('#idUsuario2').value;
+                            const idSede = modal_update.querySelector('#idSede2').value;
+
+                            const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+                            fetch(`listamuestras/update/${id}`, {
+                                method: "PUT",
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'X-CSRF-TOKEN': token
+                                },
+                                body: JSON.stringify({
+                                    fecha: fecha,
+                                    codigo: codigo,
+                                    organo: valueOrgano,
+                                    idTipo: idTipo,
+                                    idFormato: idFormato,
+                                    idCalidad: idCalidad,
+                                    idUsuario: idUsuario,
+                                    idSede: idSede,
+                                }),
+                            })
+                            .then(respuesta => {
+                                if (!respuesta.ok) {
+                                    return respuesta.text().then(err => {
+                                        try {
+                                            const jsonError = JSON.parse(err);
+                                            throw new Error(jsonError.message || 'Error en la solicitud');
+                                        } catch (e) {
+                                            throw new Error(err || 'Error en la solicitud');
+                                        }
+                                    });
+                                }
+                                return respuesta.json();
+                            })
+                            .then(data => {
+                                Swal.fire('Éxito', 'Muestra actualizada correctamente', 'success');
+                                location.reload();
+                            })
+                            .catch(error => {
+                                console.error("Error en la petición:", error);
+                                Swal.fire('Error', error.message, 'error');
+                            });
+                        });
+                    }
+                }
+            });
+
+        })
+        .catch(error => {
+            console.error("Error en la petición:", error);
+            alert("Ocurrió un error: " + error.message);
+        });
+    });
+});
+
+
+
+  function rendermodal_update(datos) {
+    console.log("Datos recibidos por rendermodal_update:", datos);
+
+    if (!datos) {
+        console.error("No se recibieron datos para el modal");
+        return;
+    }
+
+    const modal_update = document.getElementById('modal_update');
+
+    const fecha = modal_update.querySelector('#fecha2');
+    const codigo = modal_update.querySelector('#codigo2');
+    const organo = modal_update.querySelector('#organo2');
+    const idTipo = modal_update.querySelector('#idTipo2');
+    const idFormato = modal_update.querySelector('#idFormato2');
+    const idCalidad = modal_update.querySelector('#idCalidad2');
+    const idUsuario = modal_update.querySelector('#idUsuario2');
+    const idSede = modal_update.querySelector('#idSede2');
+
+
+    if (!fecha || !codigo || !organo || !idTipo || !idFormato || !idCalidad || !idUsuario || !idSede) {
+        console.error("Elementos del modal no encontrados o incorrectos");
+        return;
+    }
+
+    fecha.value = datos.fecha || "";
+    codigo.value = datos.codigo || "";
+    idTipo.value = datos.idTipo || "";
+    idFormato.value = datos.idFormato || "";
+    idCalidad.value = datos.idCalidad || "";
+    idUsuario.value = datos.idUsuario || "";
+    idSede.value = datos.idSede || "";
+
+    return modal_update;
+}
+
+eliminar.forEach(boton => {
+    boton.addEventListener('click', () => {
+
+        const id = boton.id;
+        const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        fetch('listamuestras/destroy/'+id, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': token
+            }
+        })
+        .then(respuesta => {
+            if (!respuesta.ok) {
+                // Manejo de errores: Intenta leer el error del servidor (JSON o texto)
+                return respuesta.text().then(err => {
+                    try {
+                        const jsonError = JSON.parse(err); // Intenta parsear como JSON
+                        throw new Error(jsonError.message || 'Error en la solicitud');
+                    } catch (e) {
+                        throw new Error(err || 'Error en la solicitud'); // Si no es JSON, muestra el texto
+                    }
+                });
+            }
+            return respuesta.json(); // Si la respuesta es ok, intenta parsear como JSON
+        })
+        .then(data => {
+            console.log(data);
+            Swal.fire('Éxito', 'Muestra Eliminada correctamente', 'success');
+            location.reload()
+        })
+        .catch(error => {
+            console.error("Error en la petición:", error);
+            Swal.fire('Error', error.message, 'error');
+        });
+});
+});
