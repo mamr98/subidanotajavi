327cce0c0968732eb0642588cb339fec0930ea0a
PabloTorr03 <pablogdlt03@gmail.com>
2025-02-06 11:45:51 +0100

    Prueba 4 railway

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/muestras.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        manifest: true, // Asegúrate de que el manifiesto se genere correctamente
-        outDir: 'public/build', // Asegúrate de que los archivos se generen en esta carpeta
-        emptyOutDir: true, // Esto vacía la carpeta 'public/build' antes de generar nuevos archivos
-      },
 });
+
