54eca7fba5cd9f6e4aa85eb2382a10ed663ec666
Marcoide120 <marcosgarciacasta5000@gmail.com>
2025-02-14 18:08:29 +0100

    create de interpretaciones hecho

--- app/Http/Controllers/InterpretacionesController.php
+++ app/Http/Controllers/InterpretacionesController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Calidad;
+use App\Models\Interpretacion;
+use App\Models\Sede;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class InterpretacionesController extends Controller
@@ -13,12 +17,33 @@ class InterpretacionesController extends Controller
 
 
 
-    function create(){
+    function create(Request $request){
+
+        $interpretacion = new Interpretacion();
+
+        $interpretacion->calidad = $request->input('calidad');
+        $interpretacion->descripcionCalidad = $request->input('descripcionCalidad');
+        $interpretacion->interpretacionMuestra = $request->input('interpretacionMuestra');
+        $interpretacion->descripcionInterpretacion = $request->input('descripcionInterpretacion');
+
+        $interpretacion->save();
+
+        return response()->json(['mensaje' => 'Interpretación creada correctamente'],201);
         
     }
 
     function show(){
         
+        $interpretacion = Interpretacion::paginate(10);
+        $calidad = Calidad::all();
+        $usuarios = Usuario::all();
+        $sedes = Sede::all();
+
+        /* return view('interpretaciones')-with([
+            'interpretacion' => $interpretacion,
+            'calidad' => $calidad,
+        ]); */
+
     }
 
     function update(){
