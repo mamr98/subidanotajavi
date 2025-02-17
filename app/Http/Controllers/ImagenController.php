a52b87662389370be28d8d706bb9236787a0733e
PabloTorr03 <pablogdlt03@gmail.com>
2025-02-17 23:50:14 +0100

    Cambiar imagen usuario1

---
+++ app/Http/Controllers/ImagenController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Cloudinary\Asset\Image;
+use Illuminate\Http\Request;
+use Cloudinary\Transformation\Format;
+use Cloudinary\Transformation\Resize;
+use Cloudinary\Transformation\Delivery;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+
+class ImagenController extends Controller
+{
+    public function subirImagen(Request $request)
+{
+    try {
+        $request->validate([
+            'imagen' => 'required|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        $uploadResponse = Cloudinary::upload($request->file('imagen')->getRealPath(), [
+            'folder' => 'prueba'
+        ]);
+
+        $publicID_image = $uploadResponse->getPublicId();
+        
+        $url_image = (string)(new Image($publicID_image))
+            ->resize(Resize::scale()->width(250))
+            ->delivery(Delivery::quality(35))
+            ->delivery(Delivery::format(Format::auto()));
+
+            $usuario = Usuario::find(1); // Obtener el usuario con ID (Hay que cambiarlo segun el usuario)
+            if ($usuario) {
+                $usuario->imagen = $url_image;
+                $usuario->save(); // Guardar los cambios en la base de datos
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Usuario no encontrado'
+                ], 404);
+            }
+
+        return response()->json([
+            'success' => true,
+            'url' => $url_image
+        ]);
+
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->validator->errors()->first()
+        ], 422);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Error al subir la imagen: ' . $e->getMessage()
+        ], 500);
+    }
+}
+
+    
+}
