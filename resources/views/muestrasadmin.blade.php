653fc9511149c44fdff2e937ff40a459941ccdef
PabloTorr03 <pablogdlt03@gmail.com>
2025-02-17 23:29:39 +0100

    Dompdf

--- resources/views/muestrasadmin.blade.php
+++ resources/views/muestrasadmin.blade.php
@@ -112,7 +112,7 @@
                                         Modificar
                                     </button>
 
-                                    <form action="/laravel/subidanotajavi/public/pdf/{{$m->id}}" method="POST">
+                                    <form action="/subidanotajavi/public/pdf/{{$m->id}}" method="POST">
                                         @csrf
                                         <button style="padding: 10px 18px; margin-left:4px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;" id="{{$m->id}}" class="imprimir" type="submit">
                                             Imprimir PDF
