8686ba1712222567db18e7d9a48e86b5af2514e5
Davpp24 <pelaezperezdavid@gmail.com>
2025-02-25 20:23:46 +0100

    error vite con registro.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/muestras.js', 'resources/js/imagenes.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/muestras.js', 'resources/js/imagenes.js', 'resources/js/registro.js'],
             refresh: true,
         }),
     ],
